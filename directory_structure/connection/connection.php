32491a789f7d2e2357cb00b2bc89c86a72f44b4d
micheal <micheal@gmail.com>
2023-01-27 16:10:44 +0100

    commit

--- directory_structure/connection/connection.php
+++ directory_structure/connection/connection.php
@@ -4,11 +4,11 @@ function connection()
     $host = "localhost";
     $username = "root";
     $password = "";
-    $db = "chat_systems";
+    $db = "testing";
     $connection = mysqli_connect($host, $username, $password, $db);
     if ($connection) {
         return $connection;
     } else {
-        throw new Exception("Error processing request could not connect to db {$db}" . mysqli_connect_error(), 1);
+        throw new Exception("Error processing request could not connect to db {$db}");
     }
 }
--- directory_structure/functions/functions.php
+++ directory_structure/functions/functions.php
@@ -1,1 +1,30 @@
 <?php
+require_once(__DIR__ . "/../connection/connection.php");
+function save_user($username, $password)
+{
+    $stmt = "INSERT INTO users(username,password) VALUES ('$username','$password')";
+    $stmt = mysqli_query(connection(), $stmt);
+    if ($stmt) {
+        return true;
+    } else {
+        throw new Exception("Error Processing Request Could Not Save User", 1);
+    }
+}
+function edit_user()
+{
+}
+function delete_user()
+{
+}
+
+function get_users()
+{
+}
+$username =  "Destiny";
+$password = password_hash("destiny", PASSWORD_DEFAULT);
+try {
+    save_user($username, $password);
+    echo "registered";
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
